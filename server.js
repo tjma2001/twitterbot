e052f9c9d500e27fd92b3eedc1e6d2d8a73272d6
Taariq <taariq@figornaartjie.com>
2017-06-14 22:26:44 +0200

    Journey post integrated and connected to twitter... yikes... its working.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
     , Twit = require('twit')
     , Geocode = require('./lib/Geocode')
     , Dummy = require('./lib/Dummy')
+    , Processor = require('./lib/Processor')
+    , Client = require('./lib/Client')
     , port = 9006
     , T = new Twit({
         consumer_key: 'm3y6qK05PNWHTqmgzGOlxQ5Rl'
@@ -11,33 +13,45 @@ const express = require('express')
         , access_token_secret: 'ti573MpQSEatmGbCUaAhhnXmtghIxsggYrsMdhdqLed8n'
         , timeout_ms: 60 * 1000
     })
+    , processor = new Processor()
+    , client = new Client(processor, T)
     , myName = 'TransportMeTo'
 
-console.log(process.version)
-Dummy.startDummy(T)
+// Dummy.startDummy(T)
+const start = () => {
+    console.log("Ready")
+    var stream = T.stream('user', { user: myName})
 
-var stream = T.stream('user', { user: 'tjma2001'})
+    stream.on('tweet', function (tweet) {
+        console.log("received tweet");
+        var nameId = tweet.id_str;
+        var username = tweet.user.screen_name;
+        var json = JSON.stringify(tweet)
+        if(username.indexOf(myName) !== -1) {
+            return
+        }
+        client.processTweet(tweet)
+        // T.post('statuses/update', { in_reply_to_status_id: nameId, status: `Hello there @${username}`}, (err, data, response) => {
+        //     if(!err) {
+        //         console.log('reply sent')
+        //     } else {
+        //         console.log('reply failed')
+        //     }
+        // })
+    })
 
-stream.on('tweet', function (tweet) {
-  console.log("received tweet");
-  var nameId = tweet.id_str;
-  var username = tweet.user.screen_name;
-  var json = JSON.stringify(tweet)
-  if(username.indexOf(myName) !== -1) {
-      return
-  }
-  T.post('statuses/update', { in_reply_to_status_id: nameId, status: `Hello there @${username}`}, (err, data, response) => {
-      if(!err) {
-          console.log('reply sent')
-      } else {
-          console.log('reply failed')
-      }
-  })
-})
+    stream.on('error', err => {
+        console.log(err)
+    })
 
-stream.on('error', err => {
-    console.log(err)
-})
+}
+
+(async () => {
+    let ready = await client.getReady()
+    start()
+})()
+
+
 
 server.get('/', (req, res) => {
     console.log(req);
